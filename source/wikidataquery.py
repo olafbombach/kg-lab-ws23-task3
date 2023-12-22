fe2f6093bab12a0c51cb47567396b5bd9cf3f127
lucky-luke-98 <lujae98@gmail.com>
2023-12-22 12:19:47 +0100

    changed the wikidata query so that it outputs a json file every time the test function is called

--- source/wikidataquery.py
+++ source/wikidataquery.py
@@ -1,8 +1,8 @@
 from SPARQLWrapper import SPARQLWrapper,JSON
 import pandas as pan
+import json
 
 class WikidataQuery:
-
 
   def queryWikiData(text):
 
@@ -12,18 +12,49 @@ class WikidataQuery:
     query.setQuery(text)
     query.setReturnFormat(JSON)
     results = query.query().convert()
-    return results;
+    return results
 
   
   def queryExample():
-     result = WikidataQuery.queryWikiData("""
-      SELECT ?item ?itemLabel
-      WHERE
-      {
-       ?item wdt:P31 wd:Q146 .
-       SERVICE wikibase:label { bd:serviceParam wikibase:language "en" }
-      }
-     """)
-     return result
+      text = '''
+            SELECT ?item ?itemLabel
+            WHERE {?item wdt:P31 wd:Q146.
+            SERVICE wikibase:label { bd:serviceParam wikibase:language "en" }
+            }'''
+      result = WikidataQuery.queryWikiData(text)
+      return result
 
-print(WikidataQuery.queryExample())
+  def how_many_proceedings():
+      # How many entries exist in Wikidata that have the property: instance of 'proceedings' (Q1143604)
+      text = '''
+            SELECT (COUNT (?proceeding) AS ?count)
+            WHERE {?proceeding wdt:P31 wd:Q1143604.
+            SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }
+            }'''
+      output = WikidataQuery.queryWikiData(text)
+      result = output['results']['bindings'][0]['count']['value']
+      return result
+
+  def how_many_academic_conferences():
+      # How many entries exist in Wikidata that have the property: instance of 'academic conference' (Q2020153)
+      text = '''
+            SELECT (COUNT (?conferences) AS ?count)
+            WHERE {?conferences wdt:P31 wd:Q2020153.
+            SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }
+            }'''
+      output = WikidataQuery.queryWikiData(text)
+      result = output['results']['bindings'][0]['count']['value']
+      return result
+
+  def assess_wikidata():
+      pro = WikidataQuery.how_many_proceedings()
+      ac = WikidataQuery.how_many_academic_conferences()
+
+      assess_dict = {'number of proceedings': pro, 'number of academic conferences': ac}
+      assess_json = json.dumps(assess_dict, indent=2)
+      with open('..\\results\\wikidata_assessment.json', 'w') as outfile:
+          outfile.write(assess_json)
+
+      return assess_json
+
+print(WikidataQuery.assess_wikidata())
