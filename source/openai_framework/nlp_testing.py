5230dadf7971d109fc5a7e93cb6d0a76355a8ec6
Daniel Shaolin Quirmbach <daniel.quirmbach@rwth-aachen.de>
2024-01-03 14:11:13 +0100

    updated openai NLP

---
+++ source/openai_framework/nlp_testing.py
@@ -0,0 +1,4 @@
+from nlp import NLP
+from api_secrets import API_KEY
+
+print(NLP.conferenceNLP("The Semantic Web: 15th International Conference, ESWC 2018, Heraklion, Crete, Greece, June 3–7, 2018, Proceedings",API_KEY))
