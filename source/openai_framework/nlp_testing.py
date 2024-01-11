1927683a8c56aa29e74254133606bfb0a643ce69
Daniel Shaolin Quirmbach <daniel.quirmbach@rwth-aachen.de>
2024-01-11 15:00:50 +0100

    fine-tuned NLP with OpenAI

--- source/openai_framework/nlp_testing.py
+++ source/openai_framework/nlp_testing.py
@@ -1,4 +1,5 @@
 from nlp import NLP
 from api_secrets import API_KEY
 
-print(NLP.conferenceNLP("The Semantic Web: 15th International Conference, ESWC 2018, Heraklion, Crete, Greece, June 3–7, 2018, Proceedings",API_KEY))
+#print(NLP.conferenceNLP("The Semantic Web: 15th International Conference, ESWC 2018, Heraklion, Crete, Greece, June 3–7, 2018, Proceedings",API_KEY))
+print(NLP.conferenceNLP("ACM SIGPLAN International Conference on Object-Oriented Programming Systems, Languages, and Applications (OOPSLA)",API_KEY))
