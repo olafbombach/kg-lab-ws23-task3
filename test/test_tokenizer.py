99a0c391b421344ed8c1ccb9a31da7b3446bea5b
Christophe Haag <christophe.haag@rwth-aachen.de>
2024-03-27 10:31:04 -0700

    Just to get data for the presentation. Do not pull

--- test/test_tokenizer.py
+++ test/test_tokenizer.py
@@ -9,5 +9,6 @@ def test_onDict():
                  "Description":"21-23 March 2018 Location: Cambridge, UK"}
    tok = Tokenizer()
    tok_result = tok.tokenizeProceedings(input_dict)
+   print(tok_result)
 
    assert len(tok_result) > 2  # just a random number
